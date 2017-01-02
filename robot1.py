81c7295beabe9e6627f67b80e669ae79c3248510
Aakash Indurkhya <Aakash282@gmail.com>
2017-01-02 14:18:59 -0500

    cleanup

--- robot1.py
+++ robot1.py
@@ -6,15 +6,15 @@ print 1
 gpio.setmode(gpio.BOARD)
 
 gpio.setup(7,gpio.OUT)
-gpio.setup(11,gpio.OUT)
-gpio.setup(13,gpio.OUT)
-gpio.setup(15,gpio.OUT)
+# gpio.setup(11,gpio.OUT)
+# gpio.setup(13,gpio.OUT)
+# gpio.setup(15,gpio.OUT)
 
 print 2
 gpio.output(7,True)
-gpio.output(11,False)
-gpio.output(13,False)
-gpio.output(15,False)
+# gpio.output(11,False)
+# gpio.output(13,False)
+# gpio.output(15,False)
 
 time.sleep(0.5)
 print 3
