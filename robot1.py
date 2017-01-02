59ee9bc2cf69597092c48017512d9834f3f272a6
Aakash Indurkhya <Aakash282@gmail.com>
2017-01-02 14:09:13 -0500

    cleanup

--- robot1.py
+++ robot1.py
@@ -11,9 +11,9 @@ gpio.setup(13,gpio.OUT)
 gpio.setup(15,gpio.OUT)
 
 print 2
-gpio.output(7,False)
+gpio.output(7,True)
 gpio.output(11,True)
-gpio.output(13,True)
+gpio.output(13,False)
 gpio.output(15,False)
 
 time.sleep(0.5)
